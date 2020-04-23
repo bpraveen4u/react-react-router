3d6a3b78b557c856a812624715b2b2b1f6bb6b47
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2020-04-23 10:08:32 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, NavLink, Redirect, Prompt} from 'react-router-dom'
+import User from './components/User/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  loginHandle = () => {
+    this.setState(prevState => ({loggedIn: !prevState.loggedIn}))
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li><NavLink to="/" exact activeStyle={{color:'green'}}>Home</NavLink></li>
+            <li><NavLink to="/about" exact activeStyle={{color:'green'}}>About</NavLink></li>
+            <li><NavLink to="/users/praveen" exact activeStyle={{color:'green'}}>User Praveen</NavLink></li>
+          </ul>
+          <Prompt when={!this.state.loggedIn} message={(location) => {
+            return location.pathname.startsWith('/users') ? "Are you sure": true
+          }} />
+          <input type="button" value={this.state.loggedIn ? "logout":"login"} onClick={this.loginHandle.bind(this)} />
+          <Route path="/" exact strict render={
+            () => {
+              return(<h1>Welcome Home</h1>)
+            }
+          }/>
+
+          <Route path="/about" exact strict render={
+            () => {
+              return(<h1>Welcome About</h1>)
+            }
+          }/>
+
+          <Route path="/users/:username" exact strict render={({match}) => (
+            this.state.loggedIn ? (<User username={match.params.username}/>) : (<Redirect to="/" />)
+          )}/>
+
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/User/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function User(params) {
+    return (
+    <h1>Welcome Users {params.username}</h1>
+    )
+}
+
+export default User
